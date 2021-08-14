5d528fef3830c47caa33c4ffa846ee443d5f8565
lcaliu23 <lucia.liu@utp.ac.pa>
2021-08-13 20:48:21 -0500

    Adding logs example

---
+++ writing-logs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+'''
+# -------------------------------------------------------------------
+# Created Date: Thursday, August 12th 2021, 10:23:38 am
+# Author: Lc-Liu
+# Contact: liu.lc.ch@gmail.com
+# -----
+# Copyright (c) 2021 Lc-Liu
+# Apache License 2.0
+# -------------------------------------------------------------------
+'''
+
+
+# https://stackoverflow.com/questions/57713184/how-to-write-logging-messages-to-a-file
+
+import os
+import logging
+import datetime as dt
+import time
+
+LOG_FILE = os.getcwd() + "/logs"
+if not os.path.exists(LOG_FILE):
+    os.makedirs(LOG_FILE)
+LOG_FILE = LOG_FILE + "/log.log"
+logFormatter = logging.Formatter("%(levelname)s %(asctime)s %(message)s")
+fileHandler = logging.FileHandler("{0}".format(LOG_FILE))
+fileHandler.setFormatter(logFormatter)
+rootLogger = logging.getLogger()
+rootLogger.addHandler(fileHandler)
+rootLogger.setLevel(logging.INFO)
+
+logging.info("testing out the logging functionality")
+logging.error('error')
+logging.critical('critic')
+logging.warning('WARNING!\n')
+
+try:
+    print(10/0)
+except Exception as e:
+    logging.error('error: {}'.format(str(e)))
