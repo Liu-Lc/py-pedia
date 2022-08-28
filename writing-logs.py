b3453be4e7d67c71a7aab930da55561069e6770a
Liu-Lc <liu.lc.ch@gmail.com>
2022-08-28 12:14:41 -0500

    Added new module

--- writing-logs.py
+++ writing-logs.py
@@ -15,11 +15,12 @@
 
 # https://stackoverflow.com/questions/57713184/how-to-write-logging-messages-to-a-file
 
-import os
+import os, sys
 import logging
 import datetime as dt
 import time
 
+# Creación del archivo Log
 LOG_FILE = os.getcwd() + "/logs"
 if not os.path.exists(LOG_FILE):
     os.makedirs(LOG_FILE)
@@ -27,9 +28,15 @@ LOG_FILE = LOG_FILE + "/log.log"
 logFormatter = logging.Formatter("%(levelname)s %(asctime)s %(message)s")
 fileHandler = logging.FileHandler("{0}".format(LOG_FILE))
 fileHandler.setFormatter(logFormatter)
+stdout_handler = logging.StreamHandler(sys.stdout)
 rootLogger = logging.getLogger()
 rootLogger.addHandler(fileHandler)
+rootLogger.addHandler(stdout_handler)
 rootLogger.setLevel(logging.INFO)
+
+time.sleep(3)
+logging.info("Waited 3 seconds.")
+time.sleep(3)
 
 logging.info("testing out the logging functionality")
 logging.error('error')
@@ -39,4 +46,4 @@ logging.warning('WARNING!\n')
 try:
     print(10/0)
 except Exception as e:
-    logging.error('error: {}'.format(str(e)))
+    logging.exception('Error ocurred.')
